384f2d1bd2b2a092e880f41030cdae53006dd115
Brisaniaandrade <156109971+Brisaniaandrade@users.noreply.github.com>
2024-01-22 10:12:49 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -183,87 +183,6 @@ function pok () {
     ]
 }
 let list: Image[] = []
-let Ash = sprites.create(img`
-    . . . . . . f f f f . . . . . . 
-    . . . . f f f 2 2 f f f . . . . 
-    . . . f f f 2 2 2 2 f f f . . . 
-    . . f f f e e e e e e f f f . . 
-    . . f f e 2 2 2 2 2 2 e e f . . 
-    . . f e 2 f f f f f f 2 e f . . 
-    . . f f f f e e e e f f f f . . 
-    . f f e f b f 4 4 f b f e f f . 
-    . f e e 4 1 f d d f 1 4 e e f . 
-    . . f e e d d d d d d e e f . . 
-    . . . f e e 4 4 4 4 e e f . . . 
-    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-    . . . . . f f f f f f . . . . . 
-    . . . . . f f . . f f . . . . . 
-    `, SpriteKind.Player)
-let Misty = sprites.create(img`
-    . . . . . . 5 . 5 . . . . . . . 
-    . . . . . f 5 5 5 f f . . . . . 
-    . . . . f 1 5 2 5 1 6 f . . . . 
-    . . . f 1 6 6 6 6 6 1 6 f . . . 
-    . . . f 6 6 f f f f 6 1 f . . . 
-    . . . f 6 f f d d f f 6 f . . . 
-    . . f 6 f d f d d f d f 6 f . . 
-    . . f 6 f d 3 d d 3 d f 6 f . . 
-    . . f 6 6 f d d d d f 6 6 f . . 
-    . f 6 6 f 3 f f f f 3 f 6 6 f . 
-    . . f f d 3 5 3 3 5 3 d f f . . 
-    . . f d d f 3 5 5 3 f d d f . . 
-    . . . f f 3 3 3 3 3 3 f f . . . 
-    . . . f 3 3 5 3 3 5 3 3 f . . . 
-    . . . f f f f f f f f f f . . . 
-    . . . . . f f . . f f . . . . . 
-    `, SpriteKind.Player)
-let Brock = sprites.create(img`
-    . . . . . . . f f . . . . . . . 
-    . . . . . f f 4 4 f f . . . . . 
-    . . . . f 5 4 5 5 4 5 f . . . . 
-    . . . f e 4 5 5 5 5 4 e f . . . 
-    . . f b 3 e 4 4 4 4 e 3 b f . . 
-    . f e 3 3 3 3 3 3 3 3 3 3 e f . 
-    . f 3 3 e b 3 e e 3 b e 3 3 f . 
-    . f b 3 f f e e e e f f 3 b f . 
-    f f b b f b f e e f b f b b f f 
-    f b b b e 1 f 4 4 f 1 e b b b f 
-    . f b b f 4 4 4 4 4 e e b b f . 
-    . . f e f b d d d e 4 4 4 f . . 
-    . . e 4 c d d d d e 4 4 e f . . 
-    . . e f b b d b d d e e f . . . 
-    . . . f f 1 1 d 1 d 1 f f . . . 
-    . . . . . f b b f f f . . . . . 
-    `, SpriteKind.Player)
-let YN = sprites.create(img`
-    . f f f . f f f f . f f f . 
-    f f f f f c c c c f f f f f 
-    f f f f b c c c c b f f f f 
-    f f f c 3 c c c c 3 c f f f 
-    . f 3 3 c c c c c c 3 3 f . 
-    . f c c c c 4 4 c c c c f . 
-    . f f c c 4 4 4 4 c c f f . 
-    . f f f b f 4 4 f b f f f . 
-    . f f 4 1 f d d f 1 4 f f . 
-    . . f f d d d d d d f f . . 
-    . . e f e 4 4 4 4 e f e . . 
-    . e 4 f b 3 3 3 3 b f 4 e . 
-    . 4 d f 3 3 3 3 3 3 c d 4 . 
-    . 4 4 f 6 6 6 6 6 6 f 4 4 . 
-    . . . . f f f f f f . . . . 
-    . . . . f f . . f f . . . . 
-    `, SpriteKind.Player)
-controller.player1.moveSprite(Ash, 100, 100)
-controller.player2.moveSprite(Brock, 100, 100)
-controller.player3.moveSprite(Misty, 100, 100)
-controller.player4.moveSprite(YN, 100, 100)
-tiles.loadMap(tiles.createMap(tilemap`level1`))
-splitScreen.cameraFollowSprite(splitScreen.Camera.Camera1, Ash)
-splitScreen.cameraFollowSprite(splitScreen.Camera.Camera2, Brock)
-splitScreen.cameraFollowSprite(splitScreen.Camera.Camera3, Misty)
-splitScreen.cameraFollowSprite(splitScreen.Camera.Camera4, YN)
 list = [
 img`
     . . . . . . . . . . . . . . e e 
@@ -342,7 +261,88 @@ let ashp = sprites.create(list._pickRandom(), SpriteKind.Player)
 let brockp = sprites.create(list._pickRandom(), SpriteKind.Player)
 let mistyp = sprites.create(list._pickRandom(), SpriteKind.Player)
 let ynp = sprites.create(list._pickRandom(), SpriteKind.Player)
-ashp.follow(Ash)
-brockp.follow(Brock)
-mistyp.follow(Misty)
-ynp.follow(YN)
+let Ash = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+let Misty = sprites.create(img`
+    . . . . . . 5 . 5 . . . . . . . 
+    . . . . . f 5 5 5 f f . . . . . 
+    . . . . f 1 5 2 5 1 6 f . . . . 
+    . . . f 1 6 6 6 6 6 1 6 f . . . 
+    . . . f 6 6 f f f f 6 1 f . . . 
+    . . . f 6 f f d d f f 6 f . . . 
+    . . f 6 f d f d d f d f 6 f . . 
+    . . f 6 f d 3 d d 3 d f 6 f . . 
+    . . f 6 6 f d d d d f 6 6 f . . 
+    . f 6 6 f 3 f f f f 3 f 6 6 f . 
+    . . f f d 3 5 3 3 5 3 d f f . . 
+    . . f d d f 3 5 5 3 f d d f . . 
+    . . . f f 3 3 3 3 3 3 f f . . . 
+    . . . f 3 3 5 3 3 5 3 3 f . . . 
+    . . . f f f f f f f f f f . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+let Brock = sprites.create(img`
+    . . . . . . . f f . . . . . . . 
+    . . . . . f f 4 4 f f . . . . . 
+    . . . . f 5 4 5 5 4 5 f . . . . 
+    . . . f e 4 5 5 5 5 4 e f . . . 
+    . . f b 3 e 4 4 4 4 e 3 b f . . 
+    . f e 3 3 3 3 3 3 3 3 3 3 e f . 
+    . f 3 3 e b 3 e e 3 b e 3 3 f . 
+    . f b 3 f f e e e e f f 3 b f . 
+    f f b b f b f e e f b f b b f f 
+    f b b b e 1 f 4 4 f 1 e b b b f 
+    . f b b f 4 4 4 4 4 e e b b f . 
+    . . f e f b d d d e 4 4 4 f . . 
+    . . e 4 c d d d d e 4 4 e f . . 
+    . . e f b b d b d d e e f . . . 
+    . . . f f 1 1 d 1 d 1 f f . . . 
+    . . . . . f b b f f f . . . . . 
+    `, SpriteKind.Player)
+let YN = sprites.create(img`
+    . f f f . f f f f . f f f . 
+    f f f f f c c c c f f f f f 
+    f f f f b c c c c b f f f f 
+    f f f c 3 c c c c 3 c f f f 
+    . f 3 3 c c c c c c 3 3 f . 
+    . f c c c c 4 4 c c c c f . 
+    . f f c c 4 4 4 4 c c f f . 
+    . f f f b f 4 4 f b f f f . 
+    . f f 4 1 f d d f 1 4 f f . 
+    . . f f d d d d d d f f . . 
+    . . e f e 4 4 4 4 e f e . . 
+    . e 4 f b 3 3 3 3 b f 4 e . 
+    . 4 d f 3 3 3 3 3 3 c d 4 . 
+    . 4 4 f 6 6 6 6 6 6 f 4 4 . 
+    . . . . f f f f f f . . . . 
+    . . . . f f . . f f . . . . 
+    `, SpriteKind.Player)
+ashp.follow(Ash, 40)
+brockp.follow(Brock, 40)
+mistyp.follow(Misty, 40)
+ynp.follow(YN, 40)
+controller.player1.moveSprite(Ash, 100, 100)
+controller.player2.moveSprite(Brock, 100, 100)
+controller.player3.moveSprite(Misty, 100, 100)
+controller.player4.moveSprite(YN, 100, 100)
+tiles.loadMap(tiles.createMap(tilemap`level1`))
+splitScreen.cameraFollowSprite(splitScreen.Camera.Camera1, Ash)
+splitScreen.cameraFollowSprite(splitScreen.Camera.Camera2, Brock)
+splitScreen.cameraFollowSprite(splitScreen.Camera.Camera3, Misty)
+splitScreen.cameraFollowSprite(splitScreen.Camera.Camera4, YN)
