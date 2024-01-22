15b683f34d2733a561c5fbe38cb2269eeeb51fef
HaileyAndrade <156109777+HaileyAndrade@users.noreply.github.com>
2024-01-22 10:09:06 -0600

    Update pxt.json, main.py

---
+++ main.py
@@ -0,0 +1,350 @@
+def pok():
+    global list2
+    list2 = [img("""
+            . . . . c c c b b b b b . . . . 
+                    . . c c b 4 4 4 4 4 4 b b b . . 
+                    . c c 4 4 4 4 4 5 4 4 4 4 b c . 
+                    . e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
+                    e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
+                    e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
+                    e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
+                    . e b 4 4 4 4 4 5 4 4 4 4 b e . 
+                    8 7 e e b 4 4 4 4 4 4 b e e 6 8 
+                    8 7 2 e e e e e e e e e e 2 7 8 
+                    e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
+                    e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
+                    e b e 8 8 c c 8 8 c c c 8 e b e 
+                    e e b e c c e e e e e c e b e e 
+                    . e e b b 4 4 4 4 4 4 4 4 e e . 
+                    . . . c c c c c e e e e e . . .
+        """),
+        img("""
+            . . . . . . . e c 7 . . . . . . 
+                    . . . . e e e c 7 7 e e . . . . 
+                    . . c e e e e c 7 e 2 2 e e . . 
+                    . c e e e e e c 6 e e 2 2 2 e . 
+                    . c e e e 2 e c c 2 4 5 4 2 e . 
+                    c e e e 2 2 2 2 2 2 4 5 5 2 2 e 
+                    c e e 2 2 2 2 2 2 2 2 4 4 2 2 e 
+                    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+                    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+                    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+                    c e e 2 2 2 2 2 2 2 2 2 2 4 2 e 
+                    . e e e 2 2 2 2 2 2 2 2 2 4 e . 
+                    . 2 e e 2 2 2 2 2 2 2 2 4 2 e . 
+                    . . 2 e e 2 2 2 2 2 4 4 2 e . . 
+                    . . . 2 2 e e 4 4 4 2 e e . . . 
+                    . . . . . 2 2 e e e e . . . . .
+        """),
+        img("""
+            4 4 4 . . 4 4 4 4 4 . . . . . . 
+                    4 5 5 4 4 5 5 5 5 5 4 4 . . . . 
+                    b 4 5 5 1 5 1 1 1 5 5 5 4 . . . 
+                    . b 5 5 5 5 1 1 5 5 1 1 5 4 . . 
+                    . b d 5 5 5 5 5 5 5 5 1 1 5 4 . 
+                    b 4 5 5 5 5 5 5 5 5 5 5 1 5 4 . 
+                    c d 5 5 5 5 5 5 5 5 5 5 5 5 5 4 
+                    c d 4 5 5 5 5 5 5 5 5 5 5 1 5 4 
+                    c 4 5 5 5 d 5 5 5 5 5 5 5 5 5 4 
+                    c 4 d 5 4 5 d 5 5 5 5 5 5 5 5 4 
+                    . c 4 5 5 5 5 d d d 5 5 5 5 5 b 
+                    . c 4 d 5 4 5 d 4 4 d 5 5 5 4 c 
+                    . . c 4 4 d 4 4 4 4 4 d d 5 d c 
+                    . . . c 4 4 4 4 4 4 4 4 5 5 5 4 
+                    . . . . c c b 4 4 4 b b 4 5 4 4 
+                    . . . . . . c c c c c c b b 4 .
+        """),
+        img("""
+            . . 2 2 b b b b b . . . . . . . 
+                    . 2 b 4 4 4 4 4 4 b . . . . . . 
+                    2 2 4 4 4 4 d d 4 4 b . . . . . 
+                    2 b 4 4 4 4 4 4 d 4 b . . . . . 
+                    2 b 4 4 4 4 4 4 4 d 4 b . . . . 
+                    2 b 4 4 4 4 4 4 4 4 4 b . . . . 
+                    2 b 4 4 4 4 4 4 4 4 4 e . . . . 
+                    2 2 b 4 4 4 4 4 4 4 b e . . . . 
+                    . 2 b b b 4 4 4 b b b e . . . . 
+                    . . e b b b b b b b e e . . . . 
+                    . . . e e b 4 4 b e e e b . . . 
+                    . . . . . e e e e e e b d b b . 
+                    . . . . . . . . . . . b 1 1 1 b 
+                    . . . . . . . . . . . c 1 d d b 
+                    . . . . . . . . . . . c 1 b c . 
+                    . . . . . . . . . . . . c c . .
+        """),
+        img("""
+            . . . . . . 2 2 2 2 . . . . . . 
+                    . . . . 2 2 3 3 3 3 2 e . . . . 
+                    . . . 2 3 d 1 1 d d 3 2 e . . . 
+                    . . 2 3 1 d 3 3 3 d d 3 e . . . 
+                    . 2 3 1 3 3 3 3 3 d 1 3 b e . . 
+                    . 2 1 d 3 3 3 3 d 3 3 1 3 b b . 
+                    2 3 1 d 3 3 1 1 3 3 3 1 3 4 b b 
+                    2 d 3 3 d 1 3 1 3 3 3 1 3 4 4 b 
+                    2 d 3 3 3 1 3 1 3 3 3 1 b 4 4 e 
+                    2 d 3 3 3 1 1 3 3 3 3 1 b 4 4 e 
+                    e d 3 3 3 3 d 3 3 3 d d b 4 4 e 
+                    e d d 3 3 3 d 3 3 3 1 3 b 4 b e 
+                    e 3 d 3 3 1 d d 3 d 1 b b e e . 
+                    . e 3 1 1 d d 1 1 1 b b e e e . 
+                    . . e 3 3 3 3 3 3 b e e e e . . 
+                    . . . e e e e e e e e e e . . .
+        """),
+        img("""
+            . . . . . . b b b b . . . . . . 
+                    . . . . . . b 4 4 4 b . . . . . 
+                    . . . . . . b b 4 4 4 b . . . . 
+                    . . . . . b 4 b b b 4 4 b . . . 
+                    . . . . b d 5 5 5 4 b 4 4 b . . 
+                    . . . . b 3 2 3 5 5 4 e 4 4 b . 
+                    . . . b d 2 2 2 5 7 5 4 e 4 4 e 
+                    . . . b 5 3 2 3 5 5 5 5 e e e e 
+                    . . b d 7 5 5 5 3 2 3 5 5 e e e 
+                    . . b 5 5 5 5 5 2 2 2 5 5 d e e 
+                    . b 3 2 3 5 7 5 3 2 3 5 d d e 4 
+                    . b 2 2 2 5 5 5 5 5 5 d d e 4 . 
+                    b d 3 2 d 5 5 5 d d d 4 4 . . . 
+                    b 5 5 5 5 d d 4 4 4 4 . . . . . 
+                    4 d d d 4 4 4 . . . . . . . . . 
+                    4 4 4 4 . . . . . . . . . . . .
+        """),
+        img("""
+            . . . . . . b b b b a a . . . . 
+                    . . . . b b d d d 3 3 3 a a . . 
+                    . . . b d d d 3 3 3 3 3 3 a a . 
+                    . . b d d 3 3 3 3 3 3 3 3 3 a . 
+                    . b 3 d 3 3 3 3 3 b 3 3 3 3 a b 
+                    . b 3 3 3 3 3 a a 3 3 3 3 3 a b 
+                    b 3 3 3 3 3 a a 3 3 3 3 d a 4 b 
+                    b 3 3 3 3 b a 3 3 3 3 3 d a 4 b 
+                    b 3 3 3 3 3 3 3 3 3 3 d a 4 4 e 
+                    a 3 3 3 3 3 3 3 3 3 d a 4 4 4 e 
+                    a 3 3 3 3 3 3 3 d d a 4 4 4 e . 
+                    a a 3 3 3 d d d a a 4 4 4 e e . 
+                    . e a a a a a a 4 4 4 4 e e . . 
+                    . . e e b b 4 4 4 4 b e e . . . 
+                    . . . e e e e e e e e . . . . . 
+                    . . . . . . . . . . . . . . . .
+        """),
+        img("""
+            . . . . . . . . . . b b b . . . 
+                    . . . . . . . . b e e 3 3 b . . 
+                    . . . . . . b b e 3 2 e 3 a . . 
+                    . . . . b b 3 3 e 2 2 e 3 3 a . 
+                    . . b b 3 3 3 3 3 e e 3 3 3 a . 
+                    b b 3 3 3 3 3 3 3 3 3 3 3 3 3 a 
+                    b 3 3 3 d d d d 3 3 3 3 3 d d a 
+                    b b b b b b b 3 d d d d d d 3 a 
+                    b d 5 5 5 5 d b b b a a a a a a 
+                    b 3 d d 5 5 5 5 5 5 5 d d d d a 
+                    b 3 3 3 3 3 3 d 5 5 5 d d d d a 
+                    b 3 d 5 5 5 3 3 3 3 3 3 b b b a 
+                    b b b 3 d 5 5 5 5 5 5 5 d d b a 
+                    . . . b b b 3 d 5 5 5 5 d d 3 a 
+                    . . . . . . b b b b 3 d d d b a 
+                    . . . . . . . . . . b b b a a .
+        """),
+        img("""
+            . . . . . 3 3 b 3 3 d d 3 3 . . 
+                    . . . . 3 1 1 d 3 d 1 1 1 1 3 . 
+                    . . . 3 d 1 1 1 d 1 1 1 d 3 1 3 
+                    . . 3 d d 1 1 1 d d 1 1 1 3 3 3 
+                    . 3 1 1 d 1 1 1 1 d d 1 1 b . . 
+                    . 3 1 1 1 d 1 1 1 1 1 d 1 1 3 . 
+                    . b d 1 1 1 d 1 1 1 1 1 1 1 3 . 
+                    . 4 b 1 1 1 1 d d 1 1 1 1 d 3 . 
+                    . 4 4 d 1 1 1 1 1 1 d d d b b . 
+                    . 4 d b d 1 1 1 1 1 1 1 1 3 . . 
+                    4 d d 5 b d 1 1 1 1 1 1 1 3 . . 
+                    4 5 d 5 5 b b d 1 1 1 1 d 3 . . 
+                    4 5 5 d 5 5 d b b b d d 3 . . . 
+                    4 5 5 5 d d d d 4 4 b 3 . . . . 
+                    . 4 5 5 5 4 4 4 . . . . . . . . 
+                    . . 4 4 4 . . . . . . . . . . .
+        """),
+        img("""
+            . . . . . . . 6 . . . . . . . . 
+                    . . . . . . 8 6 6 . . . 6 8 . . 
+                    . . . e e e 8 8 6 6 . 6 7 8 . . 
+                    . . e 2 2 2 2 e 8 6 6 7 6 . . . 
+                    . e 2 2 4 4 2 7 7 7 7 7 8 6 . . 
+                    . e 2 4 4 2 6 7 7 7 6 7 6 8 8 . 
+                    e 2 4 5 2 2 6 7 7 6 2 7 7 6 . . 
+                    e 2 4 4 2 2 6 7 6 2 2 6 7 7 6 . 
+                    e 2 4 2 2 2 6 6 2 2 2 e 7 7 6 . 
+                    e 2 4 2 2 4 2 2 2 4 2 2 e 7 6 . 
+                    e 2 4 2 2 2 2 2 2 2 2 2 e c 6 . 
+                    e 2 2 2 2 2 2 2 4 e 2 e e c . . 
+                    e e 2 e 2 2 4 2 2 e e e c . . . 
+                    e e e e 2 e 2 2 e e e c . . . . 
+                    e e e 2 e e c e c c c . . . . . 
+                    . c c c c c c c . . . . . . . .
+        """)]
+list2: List[Image] = []
+Ash = sprites.create(img("""
+        . . . . . . f f f f . . . . . . 
+            . . . . f f f 2 2 f f f . . . . 
+            . . . f f f 2 2 2 2 f f f . . . 
+            . . f f f e e e e e e f f f . . 
+            . . f f e 2 2 2 2 2 2 e e f . . 
+            . . f e 2 f f f f f f 2 e f . . 
+            . . f f f f e e e e f f f f . . 
+            . f f e f b f 4 4 f b f e f f . 
+            . f e e 4 1 f d d f 1 4 e e f . 
+            . . f e e d d d d d d e e f . . 
+            . . . f e e 4 4 4 4 e e f . . . 
+            . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+            . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+            . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . f f . . f f . . . . .
+    """),
+    SpriteKind.player)
+Misty = sprites.create(img("""
+        . . . . . . 5 . 5 . . . . . . . 
+            . . . . . f 5 5 5 f f . . . . . 
+            . . . . f 1 5 2 5 1 6 f . . . . 
+            . . . f 1 6 6 6 6 6 1 6 f . . . 
+            . . . f 6 6 f f f f 6 1 f . . . 
+            . . . f 6 f f d d f f 6 f . . . 
+            . . f 6 f d f d d f d f 6 f . . 
+            . . f 6 f d 3 d d 3 d f 6 f . . 
+            . . f 6 6 f d d d d f 6 6 f . . 
+            . f 6 6 f 3 f f f f 3 f 6 6 f . 
+            . . f f d 3 5 3 3 5 3 d f f . . 
+            . . f d d f 3 5 5 3 f d d f . . 
+            . . . f f 3 3 3 3 3 3 f f . . . 
+            . . . f 3 3 5 3 3 5 3 3 f . . . 
+            . . . f f f f f f f f f f . . . 
+            . . . . . f f . . f f . . . . .
+    """),
+    SpriteKind.player)
+Brock = sprites.create(img("""
+        . . . . . . . f f . . . . . . . 
+            . . . . . f f 4 4 f f . . . . . 
+            . . . . f 5 4 5 5 4 5 f . . . . 
+            . . . f e 4 5 5 5 5 4 e f . . . 
+            . . f b 3 e 4 4 4 4 e 3 b f . . 
+            . f e 3 3 3 3 3 3 3 3 3 3 e f . 
+            . f 3 3 e b 3 e e 3 b e 3 3 f . 
+            . f b 3 f f e e e e f f 3 b f . 
+            f f b b f b f e e f b f b b f f 
+            f b b b e 1 f 4 4 f 1 e b b b f 
+            . f b b f 4 4 4 4 4 e e b b f . 
+            . . f e f b d d d e 4 4 4 f . . 
+            . . e 4 c d d d d e 4 4 e f . . 
+            . . e f b b d b d d e e f . . . 
+            . . . f f 1 1 d 1 d 1 f f . . . 
+            . . . . . f b b f f f . . . . .
+    """),
+    SpriteKind.player)
+YN = sprites.create(img("""
+        . f f f . f f f f . f f f . 
+            f f f f f c c c c f f f f f 
+            f f f f b c c c c b f f f f 
+            f f f c 3 c c c c 3 c f f f 
+            . f 3 3 c c c c c c 3 3 f . 
+            . f c c c c 4 4 c c c c f . 
+            . f f c c 4 4 4 4 c c f f . 
+            . f f f b f 4 4 f b f f f . 
+            . f f 4 1 f d d f 1 4 f f . 
+            . . f f d d d d d d f f . . 
+            . . e f e 4 4 4 4 e f e . . 
+            . e 4 f b 3 3 3 3 b f 4 e . 
+            . 4 d f 3 3 3 3 3 3 c d 4 . 
+            . 4 4 f 6 6 6 6 6 6 f 4 4 . 
+            . . . . f f f f f f . . . . 
+            . . . . f f . . f f . . . .
+    """),
+    SpriteKind.player)
+controller.player1.move_sprite(Ash, 100, 100)
+controller.player2.move_sprite(Brock, 100, 100)
+controller.player3.move_sprite(Misty, 100, 100)
+controller.player4.move_sprite(YN, 100, 100)
+tiles.load_map(tiles.create_map(tilemap("""
+    level1
+""")))
+splitScreen.camera_follow_sprite(splitScreen.Camera.CAMERA1, Ash)
+splitScreen.camera_follow_sprite(splitScreen.Camera.CAMERA2, Brock)
+splitScreen.camera_follow_sprite(splitScreen.Camera.CAMERA3, Misty)
+splitScreen.camera_follow_sprite(splitScreen.Camera.CAMERA4, YN)
+list2 = [img("""
+        . . . . . . . . . . . . . . e e 
+            . . . . . . . . . . . . e e 4 5 
+            . . . . . . . . . . e e 4 5 5 5 
+            . . . . . . . . . e 4 6 7 7 6 6 
+            . . . . . . . . e 4 6 7 4 5 5 5 
+            . . . . . . . 4 4 4 8 7 4 4 4 4 
+            . . . . . . 4 5 2 2 e 7 7 7 7 7 
+            . . . . . 4 5 2 3 2 2 7 7 6 6 7 
+            . . . . 4 5 5 2 3 2 e 7 6 6 7 2 
+            . . . 4 4 5 6 7 7 7 7 5 5 4 6 2 
+            . . . e 6 6 7 7 4 5 5 4 4 7 7 e 
+            . . e 4 6 7 7 7 4 4 4 6 7 7 e 5 
+            . . e 5 6 6 8 6 7 7 6 6 6 e 5 d 
+            . e 4 5 5 4 4 e 8 7 7 6 e 5 d 5 
+            . e 5 5 4 e e e e 6 6 e 5 d 5 5 
+            . e 5 5 e e 4 4 f e e 5 d 5 d 5
+    """),
+    img("""
+        . . . . . . . . . . . . . . b b 
+            . . . . . . . . . . . b b 6 6 6 
+            . . . . . . . . . b b 3 3 6 7 7 
+            . . . . . . . . b 3 3 3 3 3 8 8 
+            . . . . . . . b 3 3 3 3 3 3 3 3 
+            . . . . . . b 3 4 4 4 3 3 3 3 3 
+            . . . . . b 3 4 5 5 4 3 3 3 3 3 
+            . . . . b 3 3 3 2 2 3 3 3 d d d 
+            . . . b 3 d 3 3 3 3 3 3 d d 3 b 
+            . . b 3 d 3 3 3 3 3 3 d 3 b b 3 
+            . . b d 3 3 3 3 3 3 3 3 b 3 3 a 
+            . b 3 d 3 6 6 3 3 3 3 b 3 3 a a 
+            . b d 3 b 9 8 3 3 3 3 a 3 a a 3 
+            . b d 6 9 8 3 3 3 3 b a a a 3 3 
+            b 3 d 6 8 3 3 3 3 3 b b a 3 3 3 
+            b d d 3 3 3 3 3 3 3 b b 3 3 3 3
+    """),
+    img("""
+        . . . . . . . . . . . 6 6 6 6 6 
+            . . . . . . . . . 6 6 7 7 7 7 8 
+            . . . . . . 8 8 8 7 7 8 8 6 8 8 
+            . . e e e e c 6 6 8 8 . 8 7 8 . 
+            . e 2 5 4 2 e c 8 . . . 6 7 8 . 
+            e 2 4 2 2 2 2 2 c . . . 6 7 8 . 
+            e 2 2 2 2 2 2 2 c . . . 8 6 8 . 
+            e 2 e e 2 2 2 2 e e e e c 6 8 . 
+            c 2 e e 2 2 2 2 e 2 5 4 2 c 8 . 
+            . c 2 e e e 2 e 2 4 2 2 2 2 c . 
+            . . c 2 2 2 e e 2 2 2 2 2 2 2 e 
+            . . . e c c e c 2 2 2 2 2 2 2 e 
+            . . . . . . . c 2 e e 2 2 e 2 c 
+            . . . . . . . c e e e e e e 2 c 
+            . . . . . . . . c e 2 2 2 2 c . 
+            . . . . . . . . . c c c c c . .
+    """),
+    img("""
+        . . . . . . . . . . . . . . . b 
+            . . . . . . . . . . . b b b b d 
+            . . . . . . . . b b b d 1 1 1 1 
+            . . . . . . b b d 1 1 1 1 1 1 1 
+            . . . . b b d 1 1 1 1 1 1 1 d d 
+            . . . b d 1 1 1 1 1 1 d d d 1 1 
+            . . b d 1 1 1 1 1 d d d 1 1 1 d 
+            . b d 1 1 1 1 1 d d 1 1 1 d d d 
+            . b 1 1 1 1 1 d 1 1 1 d d d 1 1 
+            b d 1 1 1 1 1 d 1 d d d 1 1 1 1 
+            b 1 1 1 1 1 d 1 d d d 1 1 1 1 1 
+            b 1 1 1 1 1 d d d d d 1 1 1 1 1 
+            b 1 1 1 1 1 d d d d d 1 1 1 1 1 
+            b 1 1 1 1 1 1 d d d d 1 1 1 1 1 
+            b d 1 1 1 1 1 1 d d d d 1 1 1 1 
+            . b 1 1 1 1 1 1 1 d d d d 1 1 1
+    """)]
+ashp = sprites.create(list2._pick_random(), SpriteKind.player)
+brockp = sprites.create(list2._pick_random(), SpriteKind.player)
+mistyp = sprites.create(list2._pick_random(), SpriteKind.player)
+ynp = sprites.create(list2._pick_random(), SpriteKind.player)
+ashp.follow(Ash)
+brockp.follow(Brock)
+mistyp.follow(Misty)
+ynp.follow(YN)
