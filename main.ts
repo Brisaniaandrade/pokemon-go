5a958cd025d700329e68f7a96935d2d415444f73
HaileyAndrade <156109777+HaileyAndrade@users.noreply.github.com>
2024-01-26 19:24:06 -0600

    Restore '1/24/2024, 6:54:32 PM Update main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -5,22 +5,18 @@ function sb () {
     statusbar.attachToSprite(ashp, 10, 1)
     statusbar.setColor(3, 15, 0)
     statusbar.value = 100
+    statusbar = statusbars.create(20, 4, StatusBarKind.EnemyHealth)
     statusbar.setLabel("HP")
     statusbar.setStatusBarFlag(StatusBarFlag.SmoothTransition, true)
+    statusbar.attachToSprite(ashd, 10, 1)
     statusbar.setColor(2, 15, 0)
     statusbar.value = 100
 }
-function sd () {
-    statusbar4 = statusbars.create(20, 4, StatusBarKind.Health)
-    statusbar4.setLabel("HP")
-    statusbar4.setStatusBarFlag(StatusBarFlag.SmoothTransition, true)
-    statusbar4.attachToSprite(ynp, 10, 1)
-    statusbar4.setColor(3, 15, 0)
-    statusbar4.value = 100
-    statusbar4.setLabel("HP")
-    statusbar4.setStatusBarFlag(StatusBarFlag.SmoothTransition, true)
-    statusbar4.setColor(2, 15, 0)
-    statusbar4.value = 100
+function pok () {
+	
+}
+function _catch () {
+	
 }
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile5`, function (sprite, location) {
     list = [
@@ -218,50 +214,16 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile5`, function (sprite, l
     tiles.placeOnRandomTile(brockd, assets.tile`myTile7`)
     tiles.placeOnRandomTile(mistyd, assets.tile`myTile8`)
     tiles.placeOnRandomTile(ynd, assets.tile`myTile9`)
-    sd()
-    pa()
-    sb()
-    pt()
 })
-function pt () {
-    statusbar3 = statusbars.create(20, 4, StatusBarKind.Health)
-    statusbar3.setLabel("HP")
-    statusbar3.setStatusBarFlag(StatusBarFlag.SmoothTransition, true)
-    statusbar3.attachToSprite(mistyp, 10, 1)
-    statusbar3.setColor(3, 15, 0)
-    statusbar3.value = 100
-    statusbar3.setLabel("HP")
-    statusbar3.setStatusBarFlag(StatusBarFlag.SmoothTransition, true)
-    statusbar3.setColor(2, 15, 0)
-    statusbar3.value = 100
-}
-function pa () {
-    statusbar2 = statusbars.create(20, 4, StatusBarKind.Health)
-    statusbar2.setLabel("HP")
-    statusbar2.setStatusBarFlag(StatusBarFlag.SmoothTransition, true)
-    statusbar2.attachToSprite(brockp, 10, 1)
-    statusbar2.setColor(3, 15, 0)
-    statusbar2.value = 100
-    statusbar2.setLabel("HP")
-    statusbar2.setStatusBarFlag(StatusBarFlag.SmoothTransition, true)
-    statusbar2.setColor(2, 15, 0)
-    statusbar2.value = 100
-}
-let statusbar2: StatusBarSprite = null
-let statusbar3: StatusBarSprite = null
 let ynd: Sprite = null
 let mistyd: Sprite = null
 let brockd: Sprite = null
 let ashd: Sprite = null
-let statusbar4: StatusBarSprite = null
 let statusbar: StatusBarSprite = null
 let YN: Sprite = null
 let Brock: Sprite = null
 let Misty: Sprite = null
 let Ash: Sprite = null
-let ynp: Sprite = null
-let mistyp: Sprite = null
-let brockp: Sprite = null
 let ashp: Sprite = null
 let list: Image[] = []
 story.setPagePauseLength(100, 200)
@@ -349,9 +311,9 @@ img`
     `
 ]
 ashp = sprites.create(list._pickRandom(), SpriteKind.Projectile)
-brockp = sprites.create(list._pickRandom(), SpriteKind.Projectile)
-mistyp = sprites.create(list._pickRandom(), SpriteKind.Projectile)
-ynp = sprites.create(list._pickRandom(), SpriteKind.Projectile)
+let brockp = sprites.create(list._pickRandom(), SpriteKind.Projectile)
+let mistyp = sprites.create(list._pickRandom(), SpriteKind.Projectile)
+let ynp = sprites.create(list._pickRandom(), SpriteKind.Projectile)
 Ash = sprites.create(img`
     . . . . . . f f f f . . . . . . 
     . . . . f f f 2 2 f f f . . . . 
