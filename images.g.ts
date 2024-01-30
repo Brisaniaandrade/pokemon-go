7910ce56f3f2b1ee7c3247ffed15566cd3895f8c
Brisaniaandrade <156109971+Brisaniaandrade@users.noreply.github.com>
2024-01-30 09:21:25 -0600

    Restore '1/29/2024, 10:41:39 AM Restore '1/27/2024, 9:35:47 PM Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts''

--- images.g.ts
+++ images.g.ts
@@ -3,25 +3,7 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "image1":
-            case "myImage":return img`
-d d d d d d d d d d 8 8 d d d d 
-d d d 8 8 d d d d d 8 8 d d d d 
-d d d 8 7 8 d d d 8 7 8 d d d d 
-d d d 8 7 6 8 d 8 8 7 8 d d d d 
-d d d 8 6 7 8 d 8 7 7 8 d 8 8 d 
-d d d 8 8 7 8 8 8 7 7 8 d 8 6 8 
-d d d 8 8 7 6 8 6 7 6 8 8 8 8 6 
-8 8 d 8 8 7 7 8 7 7 8 8 6 7 8 6 
-8 7 8 8 8 7 7 8 7 6 8 7 7 6 8 8 
-8 6 7 6 8 6 7 6 7 6 6 7 6 8 8 7 
-d 8 7 7 6 6 7 6 7 6 7 7 8 8 7 7 
-d d 8 7 7 6 7 8 7 6 7 6 8 7 7 8 
-d d d 8 7 6 7 8 6 6 7 8 6 7 8 8 
-d d d 8 7 8 7 8 6 6 8 8 6 6 8 6 
-d d d 8 6 8 6 8 6 6 8 8 6 6 8 6 
-d d d 8 6 8 6 6 6 6 8 8 6 6 8 6 
-`;
+
         }
         return null;
     })
