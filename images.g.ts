d886f5395ff6fe7644a9fe65a777fea0451efbdc
HaileyAndrade <156109777+HaileyAndrade@users.noreply.github.com>
2024-02-02 08:44:33 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -22,6 +22,274 @@ d d d 8 7 8 7 8 6 6 8 8 6 6 8 6
 d d d 8 6 8 6 8 6 6 8 8 6 6 8 6 
 d d d 8 6 8 6 6 6 6 8 8 6 6 8 6 
 `;
+            case "image14":
+            case "myImage12":return img`
+. . . . . . . . . . . 
+. . b b b . b b b . . 
+. b b 3 b b b 3 b b . 
+b b 3 3 3 b 3 3 3 b b 
+b 3 3 3 3 3 3 3 3 3 b 
+b b 3 3 3 3 3 3 3 b b 
+. b b 3 3 3 3 3 b b . 
+. . b b 3 3 3 b b . . 
+. . . . b 3 b . . . . 
+. . . . . b . . . . . 
+. . . . . . . . . . . 
+`;
+            case "image7":
+            case "myImage5":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . 8 8 . . . 8 8 . . . 
+. . . . . . 8 8 8 8 . 8 8 . . . 
+. . . . . . . 8 8 8 8 8 . . . . 
+. . . . . . . . f f f . . . . . 
+. . . . . . . . 8 f 8 . . . . . 
+. . . . . . . . f f . . . 8 . . 
+. . . 8 8 f f . f f . 8 8 8 . . 
+. . . . 8 8 f f f 8 f f f . . . 
+. . . . . . . f f 8 f . . . . . 
+. . . . . . . f f 8 8 . . . . . 
+. . . . . . f f . f 8 . . . . . 
+. . . . . 8 f . . f 8 . . . . . 
+. . . . . 8 8 . . f 8 . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image3":
+            case "myImage1":return img`
+.........................
+.........................
+.........................
+.........................
+.........................
+......77.................
+......777................
+.....7777................
+....777777...............
+....7777777..............
+...77377777..............
+...777777377666666.......
+....77777377766666666....
+...667377777666666666....
+..66666666666666666666...
+..66666666666f6666666f...
+...6666666666666666666...
+...6666666666666666666...
+...6666.6666666666.......
+...66...666666...........
+...66.....6666...........
+.........................
+.........................
+.........................
+.........................
+`;
+            case "image5":
+            case "myImage3":return img`
+.........................
+.........................
+.........................
+............9999999......
+...........999999999.....
+...........999999999.....
+...........999999999.....
+...........999f9999f.....
+...........999999999.....
+....eeeeeee999999f99.....
+....eeeeeeed999fff99.....
+....eeeeeedd9999999......
+...eeeeeeed9999999.......
+...eeeeeedd99999999......
+...eeeeedd999999999......
+..eeedddd999999999.......
+..eeeedd9999999999.......
+..eeeeed9999999999.......
+...eeeed9999999999.......
+....eeed9999999999.......
+.........999999999.......
+.........999999999.......
+.........999...999.......
+.........999...999.......
+.........................
+`;
+            case "image8":
+            case "myImage6":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . b b . . . . . . . . . 
+. . . . . b b . . . . . . . . . 
+. . . . . b c b . . . . . b . . 
+. . . . . b c b . . b b b b . . 
+. . . . . b c b . . b c c . . . 
+. . . . . b c b . b c c b . . . 
+. . . . . . b b b 2 b b . . . . 
+. . . . . . . f b b f . . . . . 
+. . . . . . . b b b b . . . . . 
+. . b b b b b b . . . . . . . . 
+. b b b b b b b . . . . . . . . 
+. b b . . . . b b b . . . . . . 
+. . . . . . . . . b . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image4":
+            case "myImage2":return img`
+..........................
+..........................
+..........................
+..........................
+..........................
+..........................
+..........................
+......22....4444..........
+.....222...4444444........
+....2552..44444444........
+....5522..44f4444f........
+....54....444444444.......
+....44....4444ff444.......
+....4......4444444........
+....4.....4444444.........
+...44..44.4444444.........
+...444..444444444.........
+...4444..44444444.4.......
+....4444.44444444444......
+.....444.444444444........
+......444444444444........
+.......44444444444........
+........4444444444........
+..........44444444........
+............4444444.......
+............44....4.......
+`;
+            case "image6":
+            case "myImage4":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . 2 2 . . 2 . . . 
+. . . . . . . . . 2 2 2 2 2 2 . 
+. . . . . . . . . . 2 4 4 2 2 . 
+. . . . . . . . . . 2 5 4 4 2 . 
+. . . . . . . . . 2 2 5 4 d d d 
+. . . . . d d d d d d d d f d f 
+. . 4 4 4 4 d d d d d . . d d d 
+. . 5 2 d d d d d d d . . . d d 
+. 5 4 2 d . . . . . d . . . . . 
+2 2 2 . d . . . . . d . . . . . 
+. 2 . . d . . . . . d d . . . . 
+. . . d d . . . . . d . . . . . 
+. . . d d . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image10":
+            case "myImage8":return img`
+f f f f f f f f f f f f f f f f f f f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 3 3 3 1 1 3 1 1 3 1 3 1 1 1 3 1 f 
+f 1 3 1 1 3 1 3 1 1 3 1 3 3 1 1 3 1 f 
+f 1 3 1 1 3 1 3 1 1 3 1 3 1 3 1 3 1 f 
+f 1 3 3 3 1 1 3 1 1 3 1 3 1 1 3 3 1 f 
+f 1 3 1 1 3 1 3 1 1 3 1 3 1 1 1 3 1 f 
+f 1 3 1 1 3 1 3 3 3 3 1 3 1 1 1 3 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f f f f f f f f f f f f f f f f f f f 
+`;
+            case "image11":
+            case "myImage9":return img`
+f f f f f f f f f f f f f f f f f f f f f f f f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 3 3 3 3 1 3 1 3 3 3 3 1 3 1 1 3 1 3 3 3 1 f 
+f 1 3 1 1 1 1 3 1 3 1 1 1 1 3 1 1 3 1 1 3 1 1 f 
+f 1 3 1 1 1 1 3 1 3 1 1 1 1 3 1 1 3 1 1 3 1 1 f 
+f 1 3 3 3 3 1 3 1 3 1 3 3 1 3 3 3 3 1 1 3 1 1 f 
+f 1 3 1 1 1 1 3 1 3 1 1 3 1 3 1 1 3 1 1 3 1 1 f 
+f 1 3 1 1 1 1 3 1 3 3 3 3 1 3 1 1 3 1 1 3 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f f f f f f f f f f f f f f f f f f f f f f f f 
+`;
+            case "image12":
+            case "myImage10":return img`
+f f f f f f f f f f f f f f f f f f f f f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 3 1 1 3 1 3 3 3 1 1 3 3 1 1 3 1 1 1 f 
+f 1 3 1 1 3 1 3 1 1 1 3 1 1 3 1 3 1 1 1 f 
+f 1 3 3 3 3 1 3 3 3 1 3 3 3 3 1 3 1 1 1 f 
+f 1 3 1 1 3 1 3 1 1 1 3 1 1 3 1 3 1 1 1 f 
+f 1 3 1 1 3 1 3 1 1 1 3 1 1 3 1 3 1 1 1 f 
+f 1 3 1 1 3 1 3 3 3 1 3 1 1 3 1 3 3 3 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f f f f f f f f f f f f f f f f f f f f f 
+`;
+            case "image13":
+            case "myImage11":return img`
+f f f f f f f f f f f f f f f f f f f f f f f f f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 3 3 1 1 1 3 1 1 3 3 3 1 1 3 3 1 3 1 1 3 1 f 
+f 1 3 1 1 1 1 3 1 3 1 1 3 1 1 3 1 1 1 3 1 1 3 1 f 
+f 1 3 1 1 1 3 1 1 1 3 1 3 1 1 3 1 1 1 3 3 3 3 1 f 
+f 1 3 1 1 1 3 3 3 3 3 1 3 1 1 3 1 1 1 3 1 1 3 1 f 
+f 1 3 1 1 1 3 1 1 1 3 1 3 1 1 3 1 1 1 3 1 1 3 1 f 
+f 1 1 3 3 1 3 1 1 1 3 1 3 1 1 1 3 3 1 3 1 1 3 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f f f f f f f f f f f f f f f f f f f f f f f f f 
+`;
+            case "image2":
+            case "myImage0":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f f 2 2 f f f . . . . 
+. . . f f f 2 2 2 2 f f f . . . 
+. . f f f e e 1 1 e e f f f . . 
+. . f f e 2 1 1 1 1 2 e e f . . 
+. . f e 2 f f f f f f 2 e f . . 
+. . f f f f e e e e f f f f . . 
+. f f f f d f d d f d f f f f . 
+. f f f d d f d d f d d e f f . 
+. . f e e d d d d d d e e f . . 
+. . . f e e d d d d e e f . . . 
+. . 8 8 8 8 1 1 1 1 8 8 8 8 . . 
+. . d d 8 8 1 2 2 1 8 f d d . . 
+. . d d f 8 4 5 5 4 8 8 d d . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+            case "image9":
+            case "myImage7":return img`
+fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+fffffcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccfffff
+fffffcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccfffff
+fffffcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccfffff
+fffffcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccfffff
+fffffcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccfffff
+fffffcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccfffff
+fffffcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccfffff
+fffffcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccfffff
+fffffcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccfffff
+fffffcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccfffff
+fffffcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccfffff
+fffffcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccfffff
+fffffcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccfffff
+fffffcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccfffff
+fffffcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccfffff
+fffffcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccfffff
+fffffcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccfffff
+fffffcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccfffff
+fffffcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccfffff
+fffffcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccfffff
+fffffcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccfffff
+fffffcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccfffff
+fffffcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccfffff
+fffffcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccfffff
+fffffcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccfffff
+fffffcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccfffff
+fffffcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccfffff
+fffffcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccfffff
+fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+`;
         }
         return null;
     })
